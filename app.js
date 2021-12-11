b51a7cc7de3d6e26b9f328025fb1434e2ed69c95
Jhok20 <jmonesruiz20@gmail.com>
2021-12-11 12:31:13 -0300

    Fixed CORS, enabled test routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use("/v1/pb", pbRoutes);
 app.use("/v1/users", userRoutes);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,12 +12,10 @@ router.get("/:id", userController.getUser);
 //update user
 router.put("/:id", userController.updateUser);
 
-/*
 //get all users --- JUST FOR TESTING
 router.get("/", userController.getAllUsers);
 
 //delete user --- JUST FOR TESTING
 router.delete("/:id", userController.deleteUser);
-*/
 
 module.exports = router;
