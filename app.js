13052ba821f74cf9c6111f23dc77b3a85137c31e
Jhok20 <jmonesruiz20@gmail.com>
2021-12-11 12:07:57 -0300

    Added CORS to consume API on the frontend

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const pbRoutes = require("./routes/pbRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use("/v1/pb", pbRoutes);
 app.use("/v1/users", userRoutes);
